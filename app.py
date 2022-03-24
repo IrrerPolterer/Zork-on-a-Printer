60270371edef3d545e70e2fc03a6fb3991833083
Paul Bütof <paul.buetof@righthandrobotics.com>
2022-03-24 18:57:43 +0100

    Implement new printer api

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
+from distutils import text_file
 from pytchat import create
 from threading import Thread, Event
 from queue import Queue, Empty
 from sys import argv
-from escpos.printer import Network, Dummy
 from pexpect import TIMEOUT, EOF
 from time import sleep
 import re
 
 import game_api as game
+from printer_api import tsp_print
 
 QUEUE_LOOKBACK = 1
 VIDEO_ID = argv[1]
-PRINTER_ADDRESS = argv[2]
+FONT = "Ubuntu Mono Bold Nerd Font Complete Mono.ttf"
+FONT = "Meslo LG L Bold Nerd Font Complete Mono.ttf"
+TEXT_WIDTH = 48
 
 chat_queue = Queue()
 terminate_event = Event()
@@ -37,9 +40,9 @@ def game_loop():
 
     while not terminate_event.is_set():
         try:
-            
+
             # start & restore the game
-            txt = game.start()
+            txt = game.start(width=TEXT_WIDTH)
             if game.restore():
                 txt = game.step("look")
                 print2paper(txt, "look", "[AUTO RECOVERY]")
@@ -49,27 +52,29 @@ def game_loop():
             # interactive game cycle
             while not terminate_event.is_set():
                 try:
-                
+
                     # retrieve latest player message
                     n = spool_messages()
                     if n > 0:
                         print(f"...skipping {n} messages...")
 
-                    author, message = chat_queue.get(timeout=5)
+                    author, message = chat_queue.get(timeout=10)
                     print(f"[{author}] \'{message}\'")
 
                     # execute game step
-                    txt=game.step(message)
+                    txt = game.step(message)
                     print2paper(txt, message, author)
 
                     # autosave
                     game.save()
 
+                    sleep(3)
+
                 except Empty:
                     print("...waiting for new messages...")
-            
+
         except (TIMEOUT, EOF) as err:
-            pass # restart game if it crashes
+            pass  # restart game if it crashes
 
 
 def spool_messages():
@@ -89,39 +94,30 @@ def print2paper(txt, cmd="", author=""):
     """
     Parse game output and send formatted text to printer
     """
-    
-    # connect to printer
-    n = Network(PRINTER_ADDRESS)
-    
+
     # print author and command
     if cmd:
-        line = author[:13] + ".." if len(author)>15 else author
-        line += " " if author else ""
-        n.set(bold=True)
-        n.text(line)
-        line = "> " + cmd + "\n"
-        n.set()
-        n.text(line)
-    
+        line = author[:13] + ".." if len(author) > 15 else author
+        line += " > " if author else "> "
+        line += cmd
+        tsp_print(line, text_width=TEXT_WIDTH, cut=False)
+
     # walk through all lines of the game-text
     location = None
     for line in txt.split("\n"):
-        n.set()
         # print game-text header with inversed colors
-        if re.match(".+[S|Score]: \d+\s+[M|Moves]: \d+", line):
+        if re.match(".+[S|Score]: -?\d+\s+[M|Moves]: -?\d+", line):
             location = re.findall(".*(?=[S|Score]:)", line)[0].strip()
-            line = " " + line
-            n.set(invert=True)
+            tsp_print(line, fg="#fff", bg="#000", text_width=TEXT_WIDTH, cut=False)
         # skip command & location echo
-        if line in [cmd, location]:
+        elif line in [cmd, location]:
             continue
         # print game-text line
-        n.text(line.ljust(48) + "\n")
-        
+        else:
+            tsp_print(line, text_width=TEXT_WIDTH, cut=False)
+
     # add empty lines for spacing and close connection
-    n.text("\n"*5)
-    n.close()
-    sleep(1)
+    tsp_print("\n"*2, text_width=TEXT_WIDTH, cut=True)
 
 
 def main():
@@ -137,6 +133,7 @@ def main():
 
     finally:
         terminate_event.set()
+        tsp_print("[stream lost]\n\n")
 
 
 main()
