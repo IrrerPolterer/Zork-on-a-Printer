91c89f19579715357044b033c992ade1fad1cfca
Paul Bütof <paul.buetof@righthandrobotics.com>
2023-02-12 00:06:05 +0100

    enable verbosity by default

--- game_api.py
+++ game_api.py
@@ -9,23 +9,31 @@ SAVEFILE = environ.get("SAVE_FILE", "zork1.sav")
 game = None
 
 
-def start(width=48):
+def start(width=48, first_cmd="verbose"):
     """
     Start the game and return the initial output
     """
     global game
-    
+
     if game != None:
         game.terminate()
-    
-    game = pexpect.spawn(f"bash -c \"$(pwd)/dfrotz -p -w {width} $(pwd)/{GAMEFILE}\"", timeout=5)
+
+    game = pexpect.spawn(
+        f'bash -c "$(pwd)/dfrotz -p -w {width} $(pwd)/{GAMEFILE}"', timeout=5
+    )
+
+    if first_cmd:
+        game.expect(">")
+        game.sendline(first_cmd)
 
     game.expect(">")
     try:
-        output = game.before.decode('utf-8').replace("\r", "").strip()
+        output = game.before.decode("utf-8").replace("\r", "").strip()
     except Exception:
         try:
-            output = str(from_bytes(game.before).best()).replace("\r", "").strip()
+            output = (
+                str(from_bytes(game.before).best()).replace("\r", "").strip()
+            )
         except Exception:
             output = "[ERROR] Oops, something broke. Let's try that again!"
 
@@ -37,15 +45,17 @@ def step(cmd):
     Execute a game command and return the output
     """
     global game
-    
+
     game.sendline(cmd)
 
     game.expect(">")
     try:
-        output = game.before.decode('utf-8').replace("\r", "").strip()
+        output = game.before.decode("utf-8").replace("\r", "").strip()
     except Exception:
         try:
-            output = str(from_bytes(game.before).best()).replace("\r", "").strip()
+            output = (
+                str(from_bytes(game.before).best()).replace("\r", "").strip()
+            )
         except Exception:
             output = "[ERROR] Oops, something broke. Let's try that again!"
 
@@ -57,7 +67,7 @@ def restore():
     Restore the game from a save file
     """
     global game
-    
+
     if os.path.isfile(SAVEFILE):
         game.sendline("restore")
         game.expect(":")
@@ -66,7 +76,6 @@ def restore():
         if "Ok." in game.before.decode("UTF-8"):
             return True
     return False
-    
 
 
 def save():
@@ -78,6 +87,9 @@ def save():
     game.sendline("save")
     game.expect(":")
     game.sendline(SAVEFILE)
-    if game.expect(["Overwrite existing file?", pexpect.TIMEOUT], timeout=0.5) == 0:
+    if (
+        game.expect(["Overwrite existing file?", pexpect.TIMEOUT], timeout=0.5)
+        == 0
+    ):
         game.sendline("y")
     game.expect(">")
